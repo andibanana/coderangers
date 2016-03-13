df50e1c794f3114b0a0398aa0e6c4f1191f195a2
FCsean <frzsk@yahoo.com>
2016-03-13 18:26:59 +0800

    Added message to send to inactive users. Hrefs should be added to possible links.

--- emails/emails.go
+++ emails/emails.go
@@ -2,8 +2,14 @@ package emails
 
 import (
 	".././dao"
+	".././judge"
+	".././problems"
+	".././skills"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"net/smtp"
+	"strconv"
 	"time"
 )
 
@@ -14,7 +20,7 @@ func SendEmailsToInactive() (err error) {
 	}
 	defer db.Close()
 
-	rows, err := db.Query(`SELECT user_account.id, email, max(timestamp) FROM
+	rows, err := db.Query(`SELECT user_account.id, username, email, max(timestamp) FROM
                         user_account
                           LEFT JOIN
                         submissions
@@ -27,9 +33,10 @@ func SendEmailsToInactive() (err error) {
 	for rows.Next() {
 		var timestamp string
 		var email string
+		var username string
 		var userID int
 		var submittime time.Time
-		err = rows.Scan(&userID, &email, &timestamp)
+		err = rows.Scan(&userID, &username, &email, &timestamp)
 		if err != nil {
 			return
 		}
@@ -38,10 +45,57 @@ func SendEmailsToInactive() (err error) {
 			return
 		}
 		duration := time.Since(submittime)
-		if duration.Hours()/24 >= 1 {
-			//get unsolved problems
-			//get skills
-			//send email
+		if duration.Hours()/24 >= 3 {
+			var unsolvedProblems []int
+			var userDataOnSkill map[string]skills.Skill
+			var problem problems.Problem
+			var suggestSkill bool
+			var skill skills.Skill
+			var unlockedSkills map[string]bool
+			unsolvedProblems, err = judge.GetUnsolvedProblems(userID)
+			if err != nil {
+				return
+			}
+			userDataOnSkill, err = skills.GetUserDataOnSkills(userID)
+			if err != nil {
+				return
+			}
+			unlockedSkills, err = skills.GetUnlockedSkills(userID)
+			if err != nil {
+				return
+			}
+			for _, element := range userDataOnSkill {
+				if element.Mastered {
+					continue
+				}
+				if element.Learned || unlockedSkills[element.ID] {
+					skill = element
+					suggestSkill = true
+					var problems []problems.Problem
+					problems, err = skills.GetProblemsInSkill(skill.ID)
+					skill.NumberOfProblems = len(problems)
+					if err != nil {
+						return
+					}
+					break
+				}
+			}
+			message := "<h1>Hi " + username + "</h1>"
+			message += "You've been inactive for a few days!<br>We want you back, here are a few things you can do!<br>"
+			if len(unsolvedProblems) != 0 {
+				problem, err = judge.GetProblem(unsolvedProblems[0])
+				message += `You can try to solve the problem <b>` + problem.Title + `</b><br>`
+			}
+			if suggestSkill {
+				message += "<h2>" + skill.Title + "</h2>"
+				message += skill.Description + "<br>"
+				if skill.Learned {
+					message += "You should try to master this skill. Solve " + strconv.Itoa(skill.NumberOfProblems-skill.Solved) + " more problems to master the skill.<br>"
+				} else {
+					message += "You should learn this skill. Solve " + strconv.Itoa(skill.NumberOfProblemsToUnlock-skill.Solved) + " more problems to learn the skill.<br>"
+				}
+			}
+			go SendEmail(email, "Hi "+username+"! You've been inactive for a few days!", message)
 		}
 	}
 	return
@@ -55,11 +109,33 @@ func SendEmailsEvery(interval time.Duration) {
 		for {
 			select {
 			case <-ticker.C:
-				SendEmailsToInactive()
+				err := SendEmailsToInactive()
+				log.Println(err)
 			case <-quit:
 				ticker.Stop()
 				return
 			}
 		}
 	}()
+}
+
+func SendEmail(to, subject, body string) (err error) {
+	// Set up authentication information.
+	auth := smtp.PlainAuth(
+		"",
+		"CodeRangers1@gmail.com",
+		"coderanger123",
+		"smtp.gmail.com",
+	)
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n\r\n"
+	err = smtp.SendMail(
+		"smtp.gmail.com:25",
+		auth,
+		"CodeRangers",
+		[]string{to},
+		[]byte("Subject: "+subject+"\r\n"+mime+body+"\r\n"),
+	)
+	return
 }
